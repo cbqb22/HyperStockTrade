68df19e338734e7be97bdc0d1dd6df5b5189da62
m_mikami <m_mikami@hammock.co.jp>
2017-08-17 08:56:47 +0900

    StockTradeのモデルをコミット。

---
+++ StockTradeSystem/MIC.Database/Models/DailyPrice.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using MIC.Database.Commons;
+using MIC.Database.Commons.Enums;
+
+namespace MIC.Database.Models
+{
+    /// <summary>
+    /// 名　　称 ： 日足データ
+    /// カテゴリ ： データ
+    /// </summary>
+    public class DailyPrice
+    {
+        private const string IndexName = "DailyPriceIndex";
+
+        /// <summary>
+        /// ID
+        /// </summary>
+        public int DailyDataId { get; set; }
+
+        /// <summary>
+        /// 始値
+        /// </summary>
+        [Required]
+        public double OpeningPrice  { get; set; }
+
+        /// <summary>
+        /// 高値
+        /// </summary>
+        [Required]
+        public double HighPrice { get; set; }
+
+        /// <summary>
+        /// 安値
+        /// </summary>
+        [Required]
+        public double LowPrice { get; set; }
+
+        /// <summary>
+        /// 終値
+        /// </summary>
+        [Required]
+        public double ClosingPrice { get; set; }
+
+
+        /// <summary>
+        /// 出来高
+        /// </summary>
+        [Required]
+        public double Volume { get; set; }
+
+
+        /// <summary>
+        /// 売買代金
+        /// </summary>
+        [Required]
+        public double Turnover { get; set; }
+
+
+        /// <summary>
+        /// 親テーブル
+        /// </summary>
+        public StockCompany StockCompany { get; set; }
+
+
+        /// <summary>
+        ///
+        /// </summary>
+        public DailyPrice()
+        {
+        }
+    }
+}
---
+++ StockTradeSystem/MIC.Database/Models/StockCompany.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using MIC.Database.Commons;
+using MIC.Database.Commons.Enums;
+using System.Collections.Generic;
+
+namespace MIC.Database.Models
+{
+    /// <summary>
+    /// 名　　称 ： 株式銘柄
+    /// カテゴリ ： データ
+    /// </summary>
+    public class StockCompany
+    {
+        // [0] 銘柄コード
+        // [1] 市場コード
+        // [2] 銘柄名称
+        private const string IndexName = "StockCompanyIndex";
+
+        /// <summary>
+        /// ID
+        /// </summary>
+        public int StockCompanyId { get; set; }
+
+        /// <summary>
+        /// 銘柄コード
+        /// </summary>
+        [Required,
+         Index(IndexName, IsUnique = true, Order = 1)]
+        public string StockCode { get; set; }
+
+        /// <summary>
+        /// 市場コード
+        /// </summary>
+        [Required,
+         Index(IndexName, IsUnique = true, Order = 2)]
+        public MarketCodes MarketCode { get; set; }
+
+        /// <summary>
+        /// 銘柄名称
+        /// </summary>
+        [Required(AllowEmptyStrings = true),
+         MaxLength(ColumnMaxLength.VersionLength)]
+        public string CompanyName { get; set; }
+
+
+        /// <summary>
+        /// 日足データ
+        /// 子テーブルへのリレーション
+        /// </summary>
+        public virtual ICollection<DailyPrice> DailyPrices { get; set; }
+
+
+        /// <summary>
+        ///
+        /// </summary>
+        public StockCompany()
+        {
+        }
+    }
+}
