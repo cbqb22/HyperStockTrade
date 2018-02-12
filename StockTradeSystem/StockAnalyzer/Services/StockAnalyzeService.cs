5c00eac8e3e2c74d54298dabfe7da80b3f6a7673
m_mikami <m_mikami@hammock.co.jp>
2018-02-13 08:38:59 +0900

    Analyze5の修正。

--- StockTradeSystem/StockAnalyzer/Services/StockAnalyzeService.cs
+++ StockTradeSystem/StockAnalyzer/Services/StockAnalyzeService.cs
@@ -179,12 +179,12 @@ namespace StockAnalyzer.Services
             {
                 var picked = context.StockCompany
                                     .GroupBy(x => new { x.MarketCode, x.StockCode, x.CompanyName })
-                                    .Select(x => new { x.Key.MarketCode, x.Key.StockCode, x.Key.CompanyName, DailyPrices = x.SelectMany(xx => xx.DailyPrices).Where(d => start <= d.DealDate && d.DealDate <= end) })
-                                    .Select(x => new { x.StockCode, x.MarketCode, x.CompanyName, x.DailyPrices, CurrentPrice = x.DailyPrices.OrderByDescending(p => p.DealDate).First().ClosingPrice, MaxPrice = x.DailyPrices.Max(m => m.ClosingPrice), MinPrice = x.DailyPrices.Min(m => m.ClosingPrice), Average10Volume = x.DailyPrices.OrderByDescending(d => d.DealDate).Take(10).Average(d => d.Volume), MaxVolume = x.DailyPrices.Max(d => d.Volume) })
+                                    .Select(x => new { x.Key.MarketCode, x.Key.StockCode, x.Key.CompanyName, DailyPrices = x.FirstOrDefault().DailyPrices.Where(d => start <= d.DealDate && d.DealDate <= end) })
+                                    .Select(x => new { x.StockCode, x.MarketCode, x.CompanyName, x.DailyPrices, CurrentPrice = x.DailyPrices.OrderByDescending(p => p.DealDate).FirstOrDefault().ClosingPrice, MaxPrice = x.DailyPrices.Max(m => m.ClosingPrice), MinPrice = x.DailyPrices.Min(m => m.ClosingPrice), Average10Volume = x.DailyPrices.OrderByDescending(d => d.DealDate).Take(10).Average(d => d.Volume), MaxVolume = x.DailyPrices.Max(d => d.Volume) })
                                     .Where(x => x.MarketCode == MarketCode.TSE_Mothers || x.MarketCode == MarketCode.JQ)
                                     //.Where(x => _minTurnover <= x.DailyPrices.Average(a => a.Turnover))
-                                    .Where(x => 30 <= x.DailyPrices.Where(d => x.Average10Volume * 20 <= d.Volume).Count())
-                                    .Where(x => x.CurrentPrice * 1.1 <= x.MaxPrice)
+                                    .Where(x => 20 <= x.DailyPrices.Where(d => x.Average10Volume * 10 <= d.Volume).Count())
+                                    .Where(x => x.CurrentPrice * 1.1 >= x.MaxPrice)
                                     .ToList()
                                     .Select(x => new PickedStockData
                                     {
