b63705446b2a14e9128b01a2e74d391482b25744
cbqb22 <shu_masafumi@yahoo.co.jp>
2018-04-28 22:08:20 +0900

    KB-Com のコードは削除。

--- StockTradeSystem/MIC.StockDataImport/StockDataImportStartup.cs
+++ StockTradeSystem/MIC.StockDataImport/StockDataImportStartup.cs
@@ -8,10 +8,6 @@ namespace MIC.StockDataImport
     {
         public static void RegisterServices()
         {
-            // KB-Com 株価データサイトは2017/12/31でサービス終了 
-            //SimpleIoc.Default.Register<IStockDataDownloadService, DailyDataDownloadService>();
-            //SimpleIoc.Default.Register<IStockDataImportService, DailyDataImportService>();
-
             SimpleIoc.Default.Register<IStockDataDownloadService, MuzinzouDataDownloadService>();
             SimpleIoc.Default.Register<IStockDataImportService, MuzinzouDailyDataImportService>();
             SimpleIoc.Default.Register<IStockDataBackupService, StockDataBackupService>();
