d6980aa0ec87f52f020c0b06826e75fa7f3ed1b7
cbqb22 <shu_masafumi@yahoo.co.jp>
2018-04-28 22:34:41 +0900

    BooleanReverseConverterの実装。IsHitTestVisibleの実装。

---
+++ StockTradeSystem/MIC.Common/Converters/BooleanReverseConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MIC.Common.Converters
+{
+    public class BooleanReverseConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType != typeof(bool))
+                throw new ArgumentException("The target must be a boolean");
+
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(bool)value;
+        }
+
+        #endregion
+    }
+}
